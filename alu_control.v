ca6d0230e51ce57415dd25e059dcd0d3a125af93
Omar El-Sherif <omar.ohm.elsherif@gmail.com>
2018-04-19 00:04:25 +0200

    fix: fixed number of bits in alu_control

--- alu_control.v
+++ alu_control.v
@@ -8,17 +8,17 @@ module	alu_control(OUT, AluOp, Funct);
   always	@	(Funct,	AluOp)
   begin
 	  if	(AluOp	==	0)
-		  OUT	=	6'b010;
+		  OUT	=	3'b010;
 		else begin
 		  if(AluOp == 01)
-			  OUT = 110;
+			  OUT = 3'b110;
 			else begin
 			  case (Funct)
-				  6'b100000	:	OUT	=	6'b010;
-				  6'b100010	:	OUT	=	6'b110;
-			    6'b100100	:	OUT	=	6'b000;
-					6'b100101	:	OUT	=	6'b001;
-					6'b101010	:	OUT	=	6'b111;
+				  6'b100000	:	OUT	=	3'b010;
+				  6'b100010	:	OUT	=	3'b110;
+			    6'b100100	:	OUT	=	3'b000;
+					6'b100101	:	OUT	=	3'b001;
+					6'b101010	:	OUT	=	3'b111;
 				endcase
 			end
 		end
